a7fdfc5ef8253862696c8da8418ae3b3a3e9dc6b
Derek Covey <derek.covey@gmail.com>
2018-07-25 14:23:21 -0700

    Create index for images

---
+++ src/assets/images.js
@@ -0,0 +1,9 @@
+const images = {
+    camera: require('./images/camera.svg'),
+    gear: require('./images/gear.svg'),
+    magGlass: require('./images/magnifying-glass.svg'),
+    recycleBook: require('./images/recycle-book.svg'),
+    recycleSymbol: require('./images/recycle-symbol.svg')
+}
+
+export default images
