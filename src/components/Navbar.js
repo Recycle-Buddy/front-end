152643cd16f14b9ebbd0fa296bc227269b4f0d46
Derek Covey <derek.covey@gmail.com>
2018-07-27 08:06:22 -0700

    Create component for navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import colors from '../assets/colors.js'
+import images from '../assets/images.js'
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <View style={styles.navbar}>
+        <View style={styles.imageContainer}>
+          <Image source={images.magGlass}/>
+        </View>
+        <View style={styles.imageContainer}>
+          <Image source={images.camera}/>
+        </View>
+        <View style={styles.imageContainer}>
+          <Image source={images.recycleBook}/>
+        </View>
+        <View style={styles.imageContainer}>
+          <Image source={images.gear}/>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  navbar: {
+    position: 'absolute', 
+    height: 100, 
+    bottom: 0, 
+    width: '100%', 
+    flexDirection: 'row', 
+    backgroundColor: colors.white,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  imageContainer: {
+    flex: .25,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default Navbar;
