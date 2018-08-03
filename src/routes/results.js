3d31029ac0d32e77e7ab2fe26b2fbc96f5fdd0aa
Robert Harshberger <hRobert12@users.noreply.github.com>
2018-08-03 14:20:28 -0700

    Tweak the results page a lot
    I also made the styling a little more consistint.

--- src/routes/results.js
+++ src/routes/results.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
+
+import colors from '../assets/colors.js'
+import images from '../assets/images.js'
+import metrics from '../themes/metrics.js'
+
+import Navbar from '../components/Navbar.js'
+import FullWidthContainer from '../components/FullWidthContainer.js'
+import LargeText from '../components/LargeText.js'
 
 class Results extends React.Component {
-  render() { //I'm not sure how to get the spacings right for this. Right now it looks kinda ugly.
-    return (
+  render() {
+    return ( //The info here is hardcoded, but we will enevtually want to make the info change depanding on the item scanned.
       <View
-        style={styles.search}>
-        <Text style={styles.textWithMargin}>That is a tin can!</Text>
-        <Text style={styles.textWithMargin}>Recycle it</Text> 
+        style={styles.results}>
+        <Text style={styles.headerUpper}>How to recycle</Text>
+        <Text style={styles.headerLower}>Beverage cans</Text>
+        <FullWidthContainer flex={containerFlex}>
+          <View style={{flexDirection: 'column'}}>
+            <Image source={images.recycleSymbol}/>
+            <Text>In recycling</Text>
+          </View>
+          {/*The fact that there is a LOT of whitespace here disturbs me*/}
+          <Text>Empty soda and bevrage cans should be rinsed first, then put in recycling</Text>
+        </FullWidthContainer>
         <Button
-          style={styles.textWithMargin}
           title='Go To Landing'
           onPress={() => this.props.navigation.navigate('Landing')}
         />
@@ -18,16 +33,23 @@ class Results extends React.Component {
   }
 }
 
+const containerFlex = 1/1;
+
 const styles = StyleSheet.create({
-  search: {
+  results: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'flex-start',
     backgroundColor: '#fff',
   },
-  textWithMargin: {
-    margin: 16,
+  headerUpper: {
+    fontSize: 15,
+    fontStyle: 'italic',
+    marginTop: 16
+  },
+  headerLower: {
+    fontSize: 30
   },
 });
 
