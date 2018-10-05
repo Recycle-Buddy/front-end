6c420b0464022f4c81eff69b6bc9ea2c17d25c35
SethDonohue <donohue.seth@gmail.com>
2018-10-05 15:29:57 -0700

    no wusing imagestore base 64, no need to read file or have extra packages

--- src/routes/expocamera.js
+++ src/routes/expocamera.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Button, Text, StyleSheet, Image, View, TouchableOpacity, ToastAndroid } from 'react-native';
-import { Svg, Camera, Permissions, ImageManipulator, FileSystem } from 'expo';
-
-import { Base64 } from 'js-base64';
+import { Button, Text, StyleSheet, Image, View, TouchableOpacity, ToastAndroid, ImageStore } from 'react-native';
+import { Svg, Camera, Permissions, ImageManipulator } from 'expo';
 
 import colors from '../assets/colors.js'
 import metrics from '../themes/metrics.js'
@@ -29,52 +27,56 @@ class CameraExample extends React.Component {
     }
   }
 
-  // fileResizeAndBase64 = async (uri) => {
-  //   const resized = await ImageResizer.createResizedImage(uri, 1000, 1000, 'JPEG', 80);
-  //   console.log('Resized Image URI: ', resized);
-  //   const content = await FileSystem.readAsStringAsync(resized);
-  //   return Base64.encode(content);
-  // }
-
-
-
   sendPicture = () => {
-    console.log('Sent URI: ', this.state.image.uri);
-    ImageManipulator.manipulate(this.state.image.uri, [{width: 500}], { compress: 0.8 })
+    let resizedImage;
+    // Seth - resizing and compressing the image before sending, other wise the base64string is
+    //        too large to send and/or takes too long.
+    // TODO: Seth - make the resize options variables that can be set with environment variables.
+    // TODO: Seth - Trigger the load state when the image is sent to show a loading graphic.
+    ImageManipulator.manipulate(this.state.image.uri, [{resize: {width: 500}}], { compress: 0.5 })
     .then((response) => {
-      console.log('Resized URI: ', response.uri);
-      return FileSystem.readAsStringAsync(response.uri);
-    })
-    .then(fileBuffer => {
-      return Base64.encode(fileBuffer);
-    })
-    .then(base64String => {
-      ToastAndroid.show("Waiting for Machine Learning Response...", ToastAndroid.SHORT);
-      fetch('https://postman-echo.com/post', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
+      resizedImage = response;
+      ImageStore.getBase64ForTag(
+        resizedImage.uri,
+        // getbase64ForTag SUCCESS callback
+        base64String => {
+          ToastAndroid.show("Waiting for Machine Learning Response...", ToastAndroid.SHORT);
+          fetch('http://192.168.0.23:8080/images/v1/recognize', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              image: {
+                imageBytes: base64String,
+              }
+            })
+          })
+            .then((response) => response.json())
+            .then(response => {
+              // Seth - Setting the response as part of the navigation params,
+              //        efectively passing the state to the next route. We are also passing
+              //        the resizedImage to be able to use it on the results route.
+              // TODO: Remove LOADING state (set to null).
+              this.props.navigation.navigate('Results', {
+                machineLearningResponse: response,
+                resizedImage: resizedImage
+              });
+            })
+            .catch((fetchError) => {
+              console.error('Fetch Error: ', fetchError);
+            });
         },
-        body: JSON.stringify({
-          image: {
-            imageBytes: base64String,
-          }
-        })
-      })
-      .then((response) => {
-        // Seth - Here we are setting the response as part of the navigation params,
-        //        efectively passing the state to the next route.
-        this.props.navigation.navigate('Results', { machineLearningResponse: response });
-      })
-      .catch((fetchError) => {
-        console.error(fetchError);
-      });
+        // getbase64ForTag FAILURE callback
+        error => console.log('ImageStore.getBase64ForTag: ', error)
+      );
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log('Manipulate Error: ', err));
   }
 
   retakePicture = () => {
+    // Seth - Reset the image state which triggers the render to use the cameraView JSX
     this.setState({image:null});
   }
 
