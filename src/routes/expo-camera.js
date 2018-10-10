4dab2b8630ed7af163d87168908f6179a5878a74
SethDonohue <donohue.seth@gmail.com>
2018-10-09 19:17:14 -0700

    cleaned up ghost code

--- src/routes/expo-camera.js
+++ src/routes/expo-camera.js
@@ -22,67 +22,12 @@ class MyCamera extends React.Component {
     if (this.camera) {
       this.camera.takePictureAsync()
         .then(image => {
-          // this.setState({ image: image });
           this.props.navigation.navigate('SendPicture', {
             image: image,
           });
         });
     }
   }
-
-  // sendPicture = () => {
-  //   let resizedImage;
-  //   // Seth - resizing and compressing the image before sending, other wise the base64string is
-  //   //        too large to send and/or takes too long.
-  //   // TODO: Seth - make the resize options variables that can be set with environment variables.
-  //   // TODO: Seth - Trigger the load state when the image is sent to show a loading graphic.
-  //   ImageManipulator.manipulate(this.state.image.uri, [{resize: {width: 500}}], { compress: 0.5 })
-  //   .then((response) => {
-  //     resizedImage = response;
-  //     ImageStore.getBase64ForTag(
-  //       resizedImage.uri,
-  //       // getbase64ForTag SUCCESS callback
-  //       base64String => {
-  //         ToastAndroid.show("Waiting for Machine Learning Response...", ToastAndroid.SHORT);
-  //         fetch('http://192.168.0.23:8080/images/v1/recognize', {
-  //           method: 'POST',
-  //           headers: {
-  //             'Accept': 'application/json',
-  //             'Content-Type': 'application/json',
-  //           },
-  //           body: JSON.stringify({
-  //             image: {
-  //               imageBytes: base64String,
-  //             }
-  //           })
-  //         })
-  //           .then((response) => response.json())
-  //           .then(response => {
-  //             // Seth - Setting the response as part of the navigation params,
-  //             //        efectively passing the state to the next route. We are also passing
-  //             //        the resizedImage to be able to use it on the results route.
-  //             // TODO: Remove LOADING state (set to null).
-  //             this.props.navigation.navigate('Results', {
-  //               machineLearningResponse: response,
-  //               resizedImage: resizedImage
-  //             });
-  //           })
-  //           .catch((fetchError) => {
-  //             console.error('Fetch Error: ', fetchError);
-  //           });
-  //       },
-  //       // getbase64ForTag FAILURE callback
-  //       error => console.log('ImageStore.getBase64ForTag: ', error)
-  //     );
-  //   })
-  //   .catch(err => console.log('Manipulate Error: ', err));
-  // }
-
-  // retakePicture = () => {
-  //   // Seth - Reset the image state which triggers the render to use the cameraView JSX
-  //   // this.setState({image:null});
-
-  // }
 
   render() {
     const { hasCameraPermission } = this.state;
