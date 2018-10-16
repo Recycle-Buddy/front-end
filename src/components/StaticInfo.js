751d938a2eba36b7cfe06792599dc1576539fbcd
SethDonohue <donohue.seth@gmail.com>
2018-10-15 23:44:46 -0700

    added touchable to staticInfo to get a new fact on touch

--- src/components/StaticInfo.js
+++ src/components/StaticInfo.js
@@ -3,30 +3,48 @@ import { StyleSheet } from 'react-native';
 
 import FullWidthContainer from './FullWidthContainer';
 import LargeText from './LargeText';
+import LargeTouchable from '../components/LargeTouchable';
 
 import recycleFacts from '../data/recycling-facts.json';
 
 class StaticInfo extends Component {
+  state = {
+    randomFact: '',
+  }
+
+  componentDidMount() {
+    this.getRandomFact();
+  }
+
   getRandomFact() {
     const randomIndex = Math.floor(Math.random() * recycleFacts.length);
-    return recycleFacts[randomIndex];
+    this.setState({
+      randomFact: recycleFacts[randomIndex]
+    })
   }
 
   render() {
     return(
-      <FullWidthContainer flex={this.props.containerFlex}>
-        <LargeText
-          style={styles.largeText}
-          text={this.getRandomFact()} />
-      </FullWidthContainer>
+        <LargeTouchable
+          onPress={() =>this.getRandomFact()}
+          styles={styles.touchable}
+          >
+          <LargeText
+            style={styles.largeText}
+            text={this.state.randomFact} />
+        </LargeTouchable>
     );
   }
 }
 
 const styles = StyleSheet.create({
   largeText: {
-    padding: "5%"
-  }
+    padding: 0,
+  },
+  touchable: {
+    margin: '1%',
+    borderColor: 'green'
+  },
 });
 
 
