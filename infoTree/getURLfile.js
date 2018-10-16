758ca2de638c37f37f21ff718d1aba29a0dfb782
edag94 <edag94@live.com>
2018-10-15 23:54:17 -0700

    bye loop

--- infoTree/getURLfile.js
+++ infoTree/getURLfile.js
@@ -1,11 +1,7 @@
 function getURLindex(label){
     //first find which URL maps to it
-    let targetURL = "";
-    for (int i = 0; i < whereDoesItGo.length; ++i){
-        let cur = whereDoesItGo[i];
-        if (cur.label === label)
-            targetURL = cur.value;
-    }
+    const targetURL= whereDoesItGo.find(el => el.label === label)
+    
     //found mapped URL, now check in URLS.js array to find which file to use
     let URLindex;
     for (int URLindex = 0; URLindex < leafURLS.length; ++URLindex){
